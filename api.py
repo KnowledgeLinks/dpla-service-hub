f60e2f9852c3d0611a453d543e34c045c800ab4d
Jeremy Nelson <jermnelson@gmail.com>
2016-08-15 17:05:56 -0600

    API now working as simple Flask app, added more functionality to CSV reader

--- api.py
+++ api.py
@@ -6,67 +6,55 @@ import json
 import xml.etree.ElementTree as etree
 import requests
 import urllib.parse
-from flask import Flask, request, render_template, Response
-from flask_restful import Resource, Api, reqparse
+from flask import abort, Flask, request, render_template, Response
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
-api = Api(app)
 
-parser = reqparse.RequestParser()
-parser.add_argument('initiate', type=str, help="OAI-PMH Harvester")
+with open("VERSION") as fo:
+    __version__ = fo.read()   
 
-@api.representation('text/xml')
-def output_xml(data, code, headers=None):
-    resp = app.make_response(data)
-    resp.headers['Content-Type'] = 'text/xml'
-    return resp
-    
+@app.route("/")
+def home():
+    return "KnowledgeLink.io's DPLA Service Hub Version {}".format(__version__)
 
 
-class StaticRepositoryGateway(Resource):
-    """Open Archives Initiative Protocol for Metadata Harvesting (OAI-PMH)
-    REST API <https://www.openarchives.org/pmh/>"""
+@app.route("/oai")
+def oai_switcher():
+    if 'initiate' in request.args:
+        return intermediation(request.args.get('initiate'))
+    return abort(500)
 
-    def __intermediation__(self, static_repo_url):
-        """Method takes a Static Repository URL and returns a repository 
-        object for use in the OAI-PMH xml template in the format of 
-        http://host:port/path/file.
 
-        Args:
-            static_repo_url(str): URL for repository
-        """
-        oai_info = {}
-        url_parts = urllib.parse.urlparse(static_repo_url)
-        institution_url = "{}://{}".format(url_parts.scheme, url_parts.netloc)
-        print("INSITUTION " + institution_url)
-        oai_info['baseURL'] = institution_url
-        identify_result = requests.post(
-            app.config.get("TRIPLESTORE_URL"),
-            data={"query": """PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+def intermediation(static_repo_url):
+    """Method takes a Static Repository URL and returns a repository 
+    object for use in the OAI-PMH xml template in the format of 
+    http://host:port/path/file.
 
+    Args:
+       static_repo_url(str): URL for repository
+    """
+    oai_info = {}
+    url_parts = urllib.parse.urlparse(static_repo_url)
+    institution_url = "{}://{}/".format(url_parts.scheme, url_parts.netloc)
+    oai_info['baseURL'] = institution_url
+    sparql_query = """PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 SELECT ?name
 WHERE {{
-    <{0}> rdfs:label ?name .
-}}""".format(institution_url)})
-        if identify_result.status_code > 300:
-            raise Exception("Cannot retrieve {}".format(institution_url))
-        oai_info['name'] = identify_result.json().get('results').get('bindings')[0]\
-                           .get('name').get('value')
-        
-        return oai_info    
-                         
-    def get(self):
-        """HTTP GET response handler"""
-        args = parser.parse_args()
-        static_repo_url = args.get('initiate')
-        if static_repo_url is not None:
-            oai_xml = render_template('oai-pmh.xml', 
-                repo=self.__intermediation__(static_repo_url))
-            return output_xml(oai_xml, None, None)
-        return {"message": "OAI-PMH Static Repository Gateway"}
-
-api.add_resource(StaticRepositoryGateway, "/oai")
-
+<{0}> rdfs:label ?name .
+}}""".format(institution_url)
+    identify_result = requests.post(
+        app.config.get("TRIPLESTORE_URL"),
+        data={"query": sparql_query,
+              "format":"json"})
+    if identify_result.status_code > 300:
+        raise Exception("Cannot retrieve {}".format(institution_url))
+    bindings = identify_result.json().get('results').get('bindings')
+    if len(bindings) < 1:
+        return abort(404)
+    oai_info['name'] = bindings[0].get('name').get('value')
+    return Response(render_template('oai-pmh.xml', repo=oai_info), mimetype="text/xml")
+  
+    
 if __name__ == '__main__':
     app.run(debug=True)
