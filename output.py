7f9f9333ee8bfb2a08c2f21509430a6efeaa8f9c
Jeremy Nelson <jermnelson@gmail.com>
2017-09-27 21:44:01 -0600

    modified output to use from command line

--- output.py
+++ output.py
@@ -1,32 +1,32 @@
 __author__ = "Jeremy Nelson"
 
+
 import datetime
 import os
 import sys
 import requests
+import click
 from zipfile import ZipFile, ZIP_DEFLATED
 from multiprocessing import Pool
 import bibcat.rml.processor as processor
 
-TRIPLESTORE_URL = "http://localhost:9999/blazegraph/sparql"
-PROJECT_BASE = "E:/2017/dpla-service-hub"
-
 BF2MAP4 = processor.SPARQLBatchProcessor(
     rml_rules=['bf-to-map4-alt.ttl'],
-    triplestore_url=TRIPLESTORE_URL)
+    triplestore_url='')
 
 CONTEXT = {"edm": "http://www.europeana.eu/schemas/edm/",
            "dcterms": "http://purl.org/dc/terms/",
 	   "org": "http://www.openarchives.org/ore/terms"}
 
-def transform(instance_iri):
-    result = requests.post(TRIPLESTORE_URL,
+def transform(triplestore_url, instance_iri):
+    result = requests.post(triplestore_url,
         data={"query": ITEM_SPARQL_TEMPLATE.format(instance_iri=instance_iri),
               "format": "json"})
     if result.status_code > 399:
         return
     item_bindings = result.json().get("results").get("bindings")
     item_iri = item_bindings[0].get("item").get('value')
+    BF2MAP4.triplestore_url = triplestore_url
     BF2MAP4.run(instance_iri=instance_iri,
                 item_iri=item_iri)
     output = BF2MAP4.output.serialize(
@@ -34,16 +34,19 @@ def transform(instance_iri):
         context=CONTEXT)
     return output
 
-        
-def processing(offset=0):
-    result = requests.post(TRIPLESTORE_URL,
+@click.command()
+@click.option("--triplestore_url")
+@click.option("--project_base")
+@click.option("--offset", help="Instance offset", default=0)        
+def processing(triplestore_url, project_base, offset=0):
+    result = requests.post(triplestore_url,
         data={"query": INSTANCES_SPARQL_TEMPLATE.format(offset),
               "format": "json"})
     bindings = result.json().get('results').get('bindings')
     file_name = "ms-{}-{:03}.zip".format(
         datetime.datetime.utcnow().toordinal(),
         offset)
-    tmp_location = os.path.join(PROJECT_BASE, 
+    tmp_location = os.path.join(project_base, 
                                 "dump/{}".format(file_name))
     
     dump_zip = ZipFile(tmp_location,
@@ -51,7 +54,7 @@ def processing(offset=0):
         compression=ZIP_DEFLATED,
         allowZip64=True)
     start = datetime.datetime.utcnow()
-    print("Starting Pool at {}".format(start))
+    click.echo("Starting Pool at {}".format(start))
     with Pool(processes=4) as pool:
         for i,row in enumerate(bindings):
             instance_iri = row.get("instance").get("value")
@@ -60,18 +63,18 @@ def processing(offset=0):
                 date = row.get("date")
             else:
                 date = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-            res = pool.apply_async(transform, (instance_iri,))
+            res = pool.apply_async(transform, (triplestore_url, instance_iri))
             if res is None:
                 continue
             dump_zip.writestr("resourecs/{}.json".format(key),
                               res.get())
             if not i%25 and i>0:
-                print(".", end="")
+                click.echo(".", nl=False)
             if not i%100:
-                print("{:,}".format(i), end="")
+                click.echo("{:,}".format(i), nl=False)
     
     end = datetime.datetime.utcnow()
-    print("Finished at {}, total time {} min".format(end,
+    click.echo("Finished at {}, total time {} min".format(end,
         (end-start).seconds / 60.0)) 
     dump_zip.close()
         
@@ -100,4 +103,7 @@ SELECT DISTINCT ?item
 WHERE {{
      ?item bf:itemOf <{instance_iri}> .
 }}"""
-    
+   
+
+if __name__ == "__main__":
+    processing() 
