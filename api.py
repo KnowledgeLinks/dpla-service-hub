00887fb82bc2b27541f642da5fff5f25ee4e106b
Mike Stabile <mstabile75@gmail.com>
2018-04-04 12:32:22 -0400

    updated to use json_qry for parsing es result

--- api.py
+++ api.py
@@ -40,6 +40,7 @@ from rdfframework.rml import RmlManager
 from rdfframework.configuration import RdfConfigManager
 from rdfframework.datamanager import DefinitionManager
 from rdfframework.datatypes import RdfNsManager
+from rdfframework.datasets import json_qry
 
 RmlManager().register_defs([('package_all', 'bibcat.maps')])
 # Define vocabulary and definition file locations
@@ -115,7 +116,7 @@ WHERE {{
 }} ORDER BY ?instance
 LIMIT 50000
 OFFSET {0}""".format(offset)
-    instances = CONNECTIONS.datastore.query(sparql) 
+    instances = CONNECTIONS.datastore.query(sparql)
     return instances
 
 
@@ -130,27 +131,33 @@ def __generate_profile__(instance_uri):
     work_iri = "{}#Work".format(instance_uri)
     work_sha1 = hashlib.sha1(work_iri.encode())
     click.echo("Work sha1 {}".format(work_sha1.hexdigest()))
+    source_filter = "bf_hasInstance.bf_hasItem.rml_map.map4_json_ld"
+    jqry_str = ".".join(['$', source_filter])
     try:
         click.echo("Before call to search")
         work_result = CONNECTIONS.search.es.get(
-            "works_v1",
+            "works",
+            "work",
             id=work_sha1.hexdigest(),
-            _source=["bf_hasInstance.bf_hasItem.rml_map.map4_json_ld"])
+            _source=[source_filter])
         click.echo("After es call")
     except NotFoundError:
         click.echo("Connection NotFoundError from ES")
         return
     except:
         click.echo("Raised error {}".format(sys.exc_info()))
-        return 
+        return
     if  work_result is None:
         click.echo("Work result is None")
         #abort(404)
         #click.echo("{}#Work missing _source".format(instance_uri))
         return
     click.echo("Work result {}".format(work_result.keys()))
-    return work_result.get("_source").get("bf_hasInstance", [])[0].\
-           get("bf_hasItem", [])[0].get("rml_map", {}).get("map4_json_ld")
+    items = json_qry(work_result.get('_source', {}),
+                     jqry_str)
+    if items:
+        return items[0]
+    return
 
 
 def __generate_resource_dump__():
@@ -195,7 +202,7 @@ def __generate_zip_file__(offset=0):
     file_name = "{}-{:03}.zip".format(
                                datetime.datetime.utcnow().toordinal(),
                                offset)
-    
+
     tmp_location = os.path.join(app.config.get("DIRECTORIES")[0].get("path"),
                                 "dump/{}".format(file_name))
     if os.path.exists(tmp_location) is True:
