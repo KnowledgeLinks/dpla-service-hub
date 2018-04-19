c872ff351dbea3cb0a2112908ef7167298996324
Mike Stabile <mstabile75@gmail.com>
2018-04-19 15:06:39 -0400

    updated load py for sparql clean-up

--- load.py
+++ load.py
@@ -5,13 +5,35 @@ import click
 from rdfframework.configuration import RdfConfigManager
 from rdfframework import rdfclass
 from rdfframework import search
+from rdfframework import sparql
+import bibcat
 import os, sys, pdb
 
 from api import generate_resource_dump, app, CONFIG_MANAGER
 
+def cleanup_data():
+    """
+    runs cleanup queries
+    """
+    queries = bibcat.sparql.cleanup.CLEANUP_QRY_SERIES
+    conn = CONFIG_MANAGER.conns.datastore
+
+    click.echo("Runnung data cleanup queries")
+    results = sparql.run_query_series(queries, conn)
+    click.echo("Finished cleanup queries")
+    for i, result in enumerate(results):
+        click.echo(sparql.read_query_notes(queries[i]))
+        click.echo(result + "\n\n")
+    click.echo("bibframe primary class counts:")
+    result = conn.query(bibcat.sparql.summary.CLASS_COUNTS, rtn_format="csv")
+    click.echo("\n".join(["{0: <16}{1: <16}".format(*item.split(","))
+                          for item in result.split("\r\n")
+                          if item]))
+
+
 def setup_dpla_indexing():
     CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
-   
+
     conf_mgr = CONFIG_MANAGER
     click.echo("Configuration Manager {}".format(conf_mgr))
     mappings = search.EsMappings()
@@ -25,11 +47,12 @@ def setup_dpla_indexing():
       idx_only_base=True)
     click.echo("Generating Resource Dump")
     resource_dump = generate_resource_dump()
-    tmp_location = os.path.join(conf_mgr.dirs.dump, 
+    tmp_location = os.path.join(conf_mgr.dirs.dump,
       "resourcedump.xml")
     with open(tmp_location, 'w+') as fo:
         fo.write(resource_dump.as_xml())
 
 
 if __name__ == "__main__":
+    cleanup_data()
     setup_dpla_indexing()
